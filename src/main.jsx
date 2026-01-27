ea1b7a4c3364eaffd66bbd7e247f6f98cb82de3d
alekseignatovich-sketch <alekseignatovich@gmail.com>
2026-01-27 05:12:31 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,12 @@
+// src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './index.css';
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css'; // ← ЭТА СТРОКА ОБЯЗАТЕЛЬНА!
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route element={<App />}>
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>
 );
